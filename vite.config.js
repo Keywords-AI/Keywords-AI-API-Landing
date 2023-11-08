ad6d0e69427ed868c3121cdd09737bba8dd33ab7
Ruifeng Zhang <failury29@gmail.com>
2023-11-07 19:59:46 -0500

    1

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Keywords-AI-API-Landing/",
+  base: "/",
   plugins: [react()],
   server: {
     port: 4200,
