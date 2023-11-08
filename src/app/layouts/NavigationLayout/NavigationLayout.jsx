9e86c5ccd4b798d2d1af50d96cd1ea215bb23691
Raymond1415926 <1415176717@qq.com>
2023-11-07 21:11:19 -0600

    update

--- src/app/layouts/NavigationLayout/NavigationLayout.jsx
+++ src/app/layouts/NavigationLayout/NavigationLayout.jsx
@@ -2,12 +2,12 @@ import { Outlet } from 'react-router-dom';
 import { Footer } from '../../components/Footer/Footer';
 import { Navbar } from '../../components/Navbar/Navbar';
 
-export function NavigationLayout() {
+export function NavigationLayout({ user }) {
   return (
     <div className="flex-col min-h-screen relative">
       <Navbar />
       <Outlet />
-      <Footer />
+      {!user?.email && <Footer />}
     </div>
   );
 }
--- src/app/pages/Landing/components/SmallCard/SmallCard.jsx
+++ src/app/pages/Landing/components/SmallCard/SmallCard.jsx
@@ -3,7 +3,7 @@ import './SmallCard.css';
 export function SmallCard({ linkIcon, title, content }) {
   return (
     <div className="gradient-out rounded-xl p-[1px]">
-      <div className="w-[382px] min-w-[358px] rounded-xl flex flex-col px-lg py-md items-start gap-sm gradient-in">
+      <div className="w-[382px] min-w-[358px] rounded-xl flex-col px-lg py-md items-start gap-sm gradient-in">
         {linkIcon}
         <div className="flex flex-col items-start gap-xxs self-stretch">
           <p className="display-xs">{title}</p>
