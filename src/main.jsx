f9cc10c9ac31b771f81e922d2fd689f65ca20573
failury <failury29@gmail.com>
2023-12-28 16:24:06 -0500

    use components styles and theme

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import * as ReactDOM from "react-dom/client";
 import { BrowserRouter, HashRouter } from "react-router-dom";
 import App from "./app/app";
-import "./index.css";
+import "./app/components/styles/index.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 const { join } = require("path");
-
+const theme = require("./src/app/components/styles/theme.js");
 export default {
   content: [
     join(
@@ -8,72 +8,6 @@ export default {
       "{src,pages,components,app}/**/*!(*.stories|*.spec).{js,jsx,html}"
     ),
   ],
-  theme: {
-    extend: {
-      fontFamily: {
-        inter: ["Inter", "sans-serif"],
-        "fira-code": ["Fira Code"],
-      },
-      spacing: {
-        xxxs: "4px",
-        xxs: "8px",
-        xs: "12px",
-        sm: "16px",
-        md: "20px",
-        lg: "32px",
-        xl: "60px",
-        xxl: "80px",
-        xxxl: "120px",
-      },
-      padding: {
-        xxxs: "4px",
-        xxs: "8px",
-        xs: "12px",
-        sm: "16px",
-        md: "20px",
-        lg: "32px",
-        xl: "60px",
-        xxl: "80px",
-        xxxl: "120px",
-      },
-      borderRadius: {
-        xs: "0px",
-        sm: "4px",
-        md: "8px",
-        lg: "18px",
-        xl: "24px",
-      },
-
-      colors: {
-        primary: "#6483F0 ",
-        complementary: "#123456",
-        success: "#73CB98",
-        error: "#F55656",
-        gray: {
-          white: "#F9FAFC",
-          black: "#000000",
-          2: "#181820",
-          3: "#3E424A",
-          4: "#B1B3BC",
-        },
-        box: "#1D1D1D",
-        resend: {
-          64: "rgba(239, 245, 255, 0.69)",
-        },
-      },
-      boxShadow: {
-        pricing: "2px 2px 10px 2px rgba(24, 24, 32, 0.40)",
-        purple: "0 0 40px rgba(143, 0, 210, 0.25)",
-        window: " 0px 0px 10px 10px rgba(255, 255, 255, 0.05)",
-      },
-      backgroundImage: {
-        highlight:
-          "linear-gradient(90deg, rgba(255, 255, 255, 0.00) 0%, rgba(0, 0, 0, 0.20) 0%, rgba(143, 143, 143, 0.67) 50%, rgba(0, 0, 0, 0.04) 100%)",
-      },
-      screens: {
-        sm: { max: "680px" },
-      },
-    },
-  },
+  theme: theme,
   plugins: [require("./flexPlugin.js")],
 };
