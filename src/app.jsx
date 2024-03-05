b3fb3d6fc55c4021eaaabe048f9f4f367297cc72
YunhaoLiu1228 <yunhaol4@illinois.edu>
2024-03-04 23:10:05 -0800

    integrate posthog

--- src/app.jsx
+++ src/app.jsx
@@ -1,14 +1,22 @@
 import { Routes } from "./Routes";
 import React from "react";
 import AuthContext from "./authentication/AuthContext";
+import { PostHogProvider } from "posthog-js/react";
+
 export function App() {
   const [user, setUser] = React.useState(null);
-
+  useEffect(() => {
+    posthog.init("phc_rL8MrmFrie28wgxtDurKt9t1bwCzChaJQ7WYaj1F1gH", {
+      api_host: "https://app.posthog.com",
+    });
+  }, []);
   return (
     <div className="bg-gray-black h-full w-full">
-      <AuthContext.Provider value={{ user, setUser }}>
-        <Routes />
-      </AuthContext.Provider>
+      <PostHogProvider client={posthog}>
+        <AuthContext.Provider value={{ user, setUser }}>
+          <Routes />
+        </AuthContext.Provider>
+      </PostHogProvider>
     </div>
   );
 }
