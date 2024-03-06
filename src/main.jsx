795b361491f3007565ed9cfe4857b3357503a86a
failury <failury29@gmail.com>
2024-03-06 00:03:39 -0500

    Add user-select: none to index.css

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import * as ReactDOM from "react-dom/client";
 import { BrowserRouter, HashRouter } from "react-router-dom";
 import App from "./app";
-import ".//components/styles/index.css";
+import "./components/styles/index.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
