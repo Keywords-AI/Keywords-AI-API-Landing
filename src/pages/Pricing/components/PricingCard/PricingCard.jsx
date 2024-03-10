2ff4a25826bd4a7004ecca844021a5c83c761c2d
failury <failury29@gmail.com>
2024-03-10 16:20:10 -0400

    Refactor PricingCard component

--- src/pages/Pricing/components/PricingCard/PricingCard.jsx
+++ src/pages/Pricing/components/PricingCard/PricingCard.jsx
@@ -50,57 +50,58 @@ export default function PricingCard({
   const navigate = useNavigate();
 
   return (
-    <div
-      onMouseEnter={() => setHover(true)}
-      onMouseLeave={() => setHover(false)}
-      className={cn(
-        "flex h-[720px] p-md flex-col items-center gap-xl flex-1 w-ful rounded-md min-w-[280px] ",
-        border,
-        bgColor
-        // hover && "shadow-pricing",
-      )}
-    >
-      <div className="flex flex-col items-start justify-center gap-xxs self-stretch">
-        <span className="self-stretch display-sm"> {title}</span>
-        <span className="text-md-regular self-stretch text-gray-4">
-          {" "}
-          {description}{" "}
-        </span>
-      </div>
-      <div className="flex flex-col items-center gap-lg self-stretch">
-        <div className="flex flex-col h-[56px] items-start self-stretch">
-          {price && (
-            <div className="flex items-end">
-              <span className="display-sm"> {price}</span>
-              {plan == "custom" && (
-                <span className="text-sm-regular text-gray-4 py-[2px]">
-                  + / month{" "}
-                </span>
-              )}
-              {plan != "custom" && (
-                <span className="text-sm-regular text-gray-4 py-[2px]">
-                  / month{" "}
-                </span>
-              )}
-            </div>
-          )}
-          {price && (
-            <span className="text-sm-regular text-gray-4">{bonus}</span>
-          )}
+    <div className="flex rounded-md  landing-card-outer">
+      <div
+        onMouseEnter={() => setHover(true)}
+        onMouseLeave={() => setHover(false)}
+        className={cn(
+          "flex h-[719px] p-md flex-col items-center gap-xl flex-1 w-full rounded-md min-w-[279px] bg-gray-black"
+        )}
+      >
+        <div className="flex flex-col items-start justify-center gap-xxs self-stretch">
+          <span className="self-stretch display-sm"> {title}</span>
+          <span className="text-md-regular self-stretch text-gray-4">
+            {" "}
+            {description}{" "}
+          </span>
         </div>
-        {/* <Button variant={buttonVar} text={buttonText} width="w-full" className="self-stretch items-center justify-center gap-xxs" /> */}
-        {button}
-      </div>
+        <div className="flex flex-col items-center gap-lg self-stretch">
+          <div className="flex flex-col h-[56px] items-start self-stretch">
+            {price && (
+              <div className="flex items-end">
+                <span className="display-sm"> {price}</span>
+                {plan == "custom" && (
+                  <span className="text-sm-regular text-gray-4 py-[2px]">
+                    + / month{" "}
+                  </span>
+                )}
+                {plan != "custom" && (
+                  <span className="text-sm-regular text-gray-4 py-[2px]">
+                    / month{" "}
+                  </span>
+                )}
+              </div>
+            )}
+            {price && (
+              <span className="text-sm-regular text-gray-4">{bonus}</span>
+            )}
+          </div>
+          {/* <Button variant={buttonVar} text={buttonText} width="w-full" className="self-stretch items-center justify-center gap-xxs" /> */}
+          {button}
+        </div>
 
-      <div className="flex flex-col items-start gap-md self-stretch">
-        <span className="text-md-md text-gray-white"> {featureTitle} </span>
-        <div className="flex flex-col items-start gap-xs">
-          {features.map((feature, index) => (
-            <div className="flex items-center gap-xs" key={index}>
-              <FeatureTick fill="fill-primary" />
-              <span className="text-md-regular text-gray-white">{feature}</span>
-            </div>
-          ))}
+        <div className="flex flex-col items-start gap-md self-stretch">
+          <span className="text-md-md text-gray-white"> {featureTitle} </span>
+          <div className="flex flex-col items-start gap-xs">
+            {features.map((feature, index) => (
+              <div className="flex items-center gap-xs" key={index}>
+                <FeatureTick fill="fill-primary" />
+                <span className="text-md-regular text-gray-white">
+                  {feature}
+                </span>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
