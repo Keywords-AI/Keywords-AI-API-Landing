79157cb8b6860db3d292b9ac1c2e912819e8f71b
YunhaoLiu1228 <yunhaol4@illinois.edu>
2024-03-21 13:47:23 -0700

    add link

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -74,7 +74,7 @@ export function Navbar() {
                 text="Community"
                 variant="header"
                 onClick={() =>
-                  window.open("https://discord.gg/rez3qVGu", "_blank")
+                  window.open("https://discord.gg/KEanfAafQQ", "_blank")
                 }
                 // active={location.pathname == "/pricing"}
               />
